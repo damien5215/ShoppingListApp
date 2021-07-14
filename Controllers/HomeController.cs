af75dc18016f5ad45206d2487e88bf00dd8a7ffa
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-07-14 20:04:55 +0100

    Implemented KO

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,51 +1,52 @@
 ﻿using ShoppingListApp.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ShoppingListApp.Controllers
 {
     public class HomeController : Controller
     {
-        private ShoppingRepository repo = ShoppingRepository.Current;
+        //private ShoppingRepository repo = ShoppingRepository.Current;
 
         public ViewResult Index()
         {
-            return View(repo.GetAll());
+            return View();
         }
 
-        public ActionResult Add(ShoppingList item) 
-        {
-            if (ModelState.IsValid)
-            {
-                repo.Add(item);
-                return RedirectToAction("Index");
-            }
-            else 
-            {
-                return View("Index");
-            }
-        }
+        //public ViewResult Index()
+        //{
+        //    return View(repo.GetAll());
+        //}
 
-        public ActionResult Remove(int Id) 
-        {
-            repo.Remove(Id);
-            return RedirectToAction("Index");
-        }
+        //public ActionResult Add(ShoppingList item) 
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        repo.Add(item);
+        //        return RedirectToAction("Index");
+        //    }
+        //    else 
+        //    {
+        //        return View("Index");
+        //    }
+        //}
 
-        public ActionResult Update(ShoppingList item) 
-        {
-            if (ModelState.IsValid && repo.Update(item))
-            {
-                return RedirectToAction("Index");
-            }
-            else 
-            {
-                return View("Index");
-            }
-        }
+        //public ActionResult Remove(int Id) 
+        //{
+        //    repo.Remove(Id);
+        //    return RedirectToAction("Index");
+        //}
+
+        //public ActionResult Update(ShoppingList item) 
+        //{
+        //    if (ModelState.IsValid && repo.Update(item))
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    else 
+        //    {
+        //        return View("Index");
+        //    }
+        //}
 
     }
 }
